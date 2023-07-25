8769dda22121657971241166f73883292897490a
Corey Hulse <cshulse@gmail.com>
2023-07-25 23:30:38 +0000

    mapping Zips with Leading Zeros

--- models/staging/stg_zip_to_dma.sql
+++ models/staging/stg_zip_to_dma.sql
@@ -1,12 +1,6 @@
 SELECT
-  zip_code
+  zip_code::text AS zip_code
 , MAX(dma_code) AS dma_code
 , MAX(dma_description) AS dma_description
 FROM {{ ref('zip_to_dma') }}
 GROUP BY 1
-
-{{
-  config({
-    "post-hook": 'ALTER TABLE {{ target.schema }}.{{ this.name }} add PRIMARY KEY(zip_code)'
-    })
-}}
